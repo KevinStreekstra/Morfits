5279aad680cfdabe1d1ec8df53c1f55cf481fe4f
Patrick Frumau <pariek@Patricks-MacBook-Pro.local>
2019-04-17 10:26:08 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -7,19 +7,27 @@ config.game.scene = {preload, create, update};
 const game = new Phaser.Game(config.game);
 
 function preload() {
-  this.load.image("logo", logoImg);
+
+  this.load.image("./layers/Background-mountain-depth-4.png", );
 }
 
 function create() {
-  const logo = this.add.image(270, 480, "logo");
+ 
 
   this.tweens.add({
     targets: logo,
-    y: 600,
+    y: 400,
     duration: 2000,
     yoyo: true,
     loop: -1,
   });
+
+ 
+  
+
 }
+
+  
+
 
 function update() {}
