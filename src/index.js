0a6a8d40abc62029df8491527da4332720ea9a9d
Kevin Streekstra <kevinstreekstra07@gmail.com>
2019-04-23 10:48:48 +0200

    Daily commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import Phaser from "phaser";
 
 import BootScene from './scenes/Boot';
 import Healthbar from './scenes/Healthbar';
+import HealthMain from './scenes/healthmain';
 import ParallaxScene from './scenes/Parallax';
 
 const game = new Phaser.Game({
     parent: 'game',
     type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
+    width: 375,
+    height: 475,
     scene: [
       BootScene,
       ParallaxScene,
+      HealthMain,
       Healthbar,
     ],
 });
