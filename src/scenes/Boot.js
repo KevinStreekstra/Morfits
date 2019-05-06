86262ee8cd3cb663c770a0222a3c2e2ecffc8bf0
Patrick Frumau <pariek@Patricks-MacBook-Pro.local>
2019-05-06 12:39:04 +0200

    Loading screen update

--- src/scenes/Boot.js
+++ src/scenes/Boot.js
@@ -1,13 +1,25 @@
 import Player from '../classes/Player';
+import AlignGrid from '../classes/AlignGrid';
 
 class BootScene extends Phaser.Scene {
     constructor() {
         super({
             key: 'BootScene'
         });
+        this.morfitLogo;
     }
 
     preload() {
+
+        this.grid = new AlignGrid({
+            scene: this, 
+            rows: 11, 
+            cols: 11, 
+            width: this.sys.game.config.width,         
+            height: this.sys.game.config.height
+        });
+      
+
         const progress = this.add.graphics();
         const progressValue = this.add.text(this.sys.game.config.width / 2, this.sys.game.config.height / 2, '0%', { fontSize: '32px', fill: '#000' });
         this.load.image('bird1', 'src/assets/boot/bird-1.png');
@@ -17,14 +29,11 @@ class BootScene extends Phaser.Scene {
         this.load.image('morfitWalking', 'src/assets/boot/Morfit walking.png');
         this.load.image('morfitLogo', 'src/assets/boot/morfit-logo.png');
         
+  
+
         this.load.on('progress', (value) => {
             progress.clear();
-            this.add.image(0, 0, 'morfitLogo').setOrigin(0, 0);
-            this.add.image(100, 100, 'morfitWalking').setOrigin(100, 100);
-            this.add.image(0, 0, 'loadingText').setOrigin(0, 0);
-            //this.add.image(0, 0, 'bird1').setOrigin(0,0);
-            //this.add.image(0, 0, 'bird2').setOrigin(0,0);
-            let test = this.add.image(0, 340, 'ground').setOrigin(0, 0);
+     
         });
 
         this.load.on('complete', () => {
@@ -37,10 +46,34 @@ class BootScene extends Phaser.Scene {
         });
 
         
+
+        
         this.overview_assets();
 
         new Player().init();
     }
+
+    create() {
+        this.morfitLogo = this.add.image(0, 0, 'morfitLogo').setOrigin(0, 0);
+        this.morfitLogo.setScale(0.5);
+
+       
+   
+        
+        //this.add.image(0, 0, 'bird1').setOrigin(0,0);
+        //this.add.image(0, 0, 'bird2').setOrigin(0,0);
+        this.ground = this.add.image(0, 0, 'ground').setScale(0.3).setOrigin(0.5, 0.35);
+        this.ground.setScale(0.5);
+        this.grid.placeAtIndex(104, this.ground);
+        this.morfitWalking = this.add.image(0, 0, 'morfitWalking').setScale(0.35);
+        this.grid.placeAtIndex(71, this.morfitWalking);
+        this.loadingText = this.add.image(0, 0, 'loadingText');
+        this.loadingText.displayWidth = 375 * 0.9;
+        this.loadingText.scaleY = this.loadingText.scaleX;
+        this.grid.placeAtIndex(115, this.loadingText);
+        // this.grid.showNumbers();
+    }
+    
 
     overview_assets() {
         this.load.image('bg', 'src/assets/bg/background.png');
