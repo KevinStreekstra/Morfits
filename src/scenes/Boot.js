0ce592a936adf0a8da019c066cf4b44bd186b096
Kevin Streekstra <kevinstreekstra07@gmail.com>
2019-05-09 10:00:58 +0200

    finished overview page

--- src/scenes/Boot.js
+++ src/scenes/Boot.js
@@ -15,8 +15,8 @@ class BootScene extends Phaser.Scene {
 
         this.grid = new AlignGrid({
             scene: this, 
-            rows: 11, 
-            cols: 11,
+            rows: 15, 
+            cols: 15,
             width: this.sys.game.config.width,         
             height: this.sys.game.config.height
         });
@@ -29,26 +29,52 @@ class BootScene extends Phaser.Scene {
         this.load.image('bird2', 'src/assets/boot/bird-2.png');
         this.load.image('loadingText', 'src/assets/boot/loading-text.png');
 
+        //morfit & morfitlogo
         this.load.image('overview:morfitWalking', 'src/assets/caracter_page/morfit_character@2x.png');
         this.load.image('morfitLogo', 'src/assets/boot/morfit-logo.png');
+        this.load.image('overview:XPbar', 'src/assets/caracter_page/exp_bar@2x.png');
+        this.load.image('overview:txtXPbar', 'src/assets/caracter_page/250-486XP@2x.png');
 
-        this.load.image('overview:HomePlant', 'src/assets/caracter_page/plant-vase@2x.png');
-        this.load.image('overview:HomePlant2', 'src/assets/caracter_page/plant-pot@2x.png');
-
+        //navbar & bars
         this.load.image('overview:MentalBar', 'src/assets/caracter_page/mental_bar@2x.png');
+        this.load.image('overview:Mental80', 'src/assets/caracter_page/80-100@2x.png');
+        this.load.image('overview:IconMental', 'src/assets/caracter_page/icon_mental_fill@2x.png');
         this.load.image('overview:EnergyBar', 'src/assets/caracter_page/energie_bar@2x.png');
+        this.load.image('overview:Energy100', 'src/assets/caracter_page/100-100@2x.png');
+        this.load.image('overview:IconEnergy', 'src/assets/caracter_page/icon_energie_bar_fill@2x.png');
         this.load.image('overview:PowerBar', 'src/assets/caracter_page/kracht_bar@2x.png');
+        this.load.image('overview:Power100', 'src/assets/caracter_page/100-100@2x.png');
+        this.load.image('overview:IconPower', 'src/assets/caracter_page/icon_kracht_fill@2x.png');
         this.load.image('overview:TopNavbar', 'src/assets/caracter_page/nav@2x.png');
 
+        //bottom navbar icons / tekst
+        this.load.image('overview:btnSettings', 'src/assets/caracter_page/btn_setting@2x.png');
+        this.load.image('overview:btnFriends', 'src/assets/caracter_page/btn_freinds@2x.png');
+        this.load.image('overview:btnGym', 'src/assets/caracter_page/brn_gym@2x.png');
+        this.load.image('overview:btnGames', 'src/assets/caracter_page/btn_games@2x.png');
+        this.load.image('overview:btnfeed', 'src/assets/caracter_page/btn_feed@2x.png');
+        this.load.image('overview:btnInventory', 'src/assets/caracter_page/btn_inventory@2x.png');
+        this.load.image('overview:btnQuiz', 'src/assets/caracter_page/btn_daily-reward@2x.png');
+
+        this.load.image('overview:txtGym', 'src/assets/caracter_page/Gym@2x.png');
+        this.load.image('overview:txtGames', 'src/assets/caracter_page/Games@2x.png');
+        this.load.image('overview:txtFeed', 'src/assets/caracter_page/Voeden@2x.png');
+        this.load.image('overview:txtInventory', 'src/assets/caracter_page/Inventaris@2x.png');
+        this.load.image('overview:txtQuiz', 'src/assets/caracter_page/Quiz@2x.png');
+
+        //bottom navbar
         this.load.image('overview:BottomNavbar', 'src/assets/caracter_page/nav-bottom_background@2x.png');
         this.load.image('overview:ground2', 'src/assets/caracter_page/ground-bottom@2x.png');
         this.load.image('overview:ground3', 'src/assets/caracter_page/ground-top@2x.png');
         
+        //plants
         this.load.image('plant1', 'src/assets/boot/Plant_2.png');
         this.load.image('plant2', 'src/assets/boot/Plant_6.png');
         this.load.image('HomePlant', 'src/assets/bg/home/Plant_7.png');
         this.load.image('HomePlant2', 'src/assets/bg/home/Plant_9.png');
         this.load.image('SkyPlant', 'src/assets/bg/home/skyPlant.png');
+        this.load.image('overview:HomePlant', 'src/assets/caracter_page/plant-vase@2x.png');
+        this.load.image('overview:HomePlant2', 'src/assets/caracter_page/plant-pot@2x.png');
   
 
         this.load.on('progress', (value) => {
