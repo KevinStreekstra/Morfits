e8b3eb4866d150ea171784cf0197e97281d1fc4a
Patrick Frumau <pariek@Patricks-MacBook-Pro.local>
2019-05-14 08:48:53 +0200

    Loading screen finished

--- src/scenes/Boot.js
+++ src/scenes/Boot.js
@@ -14,15 +14,11 @@ class BootScene extends Phaser.Scene {
     preload() {
         this.grid = new AlignGrid({
             scene: this, 
-            rows: 11, 
+            rows: 13, 
             cols: 11,
             width: this.sys.game.config.width,         
             height: this.sys.game.config.height
         });
-      
-
-        const progress = this.add.graphics();
-        const progressValue = this.add.text(this.sys.game.config.width / 2, this.sys.game.config.height / 2, '0%', { fontSize: '32px', fill: '#000' });
         
         this.load.spritesheet('boot_bird1', 'src/assets/boot/bird-1.png', {frameWidth: 400, frameHeight: 400});
         this.load.spritesheet('boot_bird2', 'src/assets/boot/bird-2.png', {frameWidth: 400, frameHeight: 400});
@@ -80,20 +76,22 @@ class BootScene extends Phaser.Scene {
         this.load.image('overview:HomePlant', 'src/assets/caracter_page/plant-vase@2x.png');
         this.load.image('overview:HomePlant2', 'src/assets/caracter_page/plant-pot@2x.png');
 
-
+        window.setInterval(()=>{
+            if ((this.boot_loadingText.text += '.').length == 25) 
+                this.boot_loadingText.text = 'Je Morfit is onderweg';
+        }, 750);
 
         this.load.on('progress', (value) => {
-            progress.clear();
-     
+            
+            
         });
 
         this.load.on('complete', () => {
-            progress.destroy();
-            if(new Player().exists()) {
-                this.scene.start('OverviewScene');
-            } else {
-                this.scene.start('CreatePlayerScene');
-            }
+            // if(new Player().exists()) {
+            //     this.scene.start('OverviewScene');
+            // } else {
+            //     this.scene.start('CreatePlayerScene');
+            // }
         });
 
         
@@ -120,16 +118,23 @@ class BootScene extends Phaser.Scene {
         });
 
         // De achtergrond van het scherm
+        
         this.bg = this.add.image(0, 0, 'bg');
         this.grid.placeAtIndex(60, this.bg); 
+        this.bg_cloud6 = this.add.image(0, 0, 'bg_cloud6').setOrigin(0, 0.19);
         this.mntn1 = this.add.image(0, 0, 'bg_mntn1').setOrigin(0, 0);
         this.grid.scaleX(this.mntn1, 2.5);
-        this.mntn2 = this.add.image(0, 0, 'bg_mntn2').setOrigin(0.13, 0);
-        this.bg_trees1 = this.add.image(0, 0, 'bg_trees1').setOrigin(0.13, 0);
-        
        
-        
-    
+        this.mntn2 = this.add.image(0, 0, 'bg_mntn2').setOrigin(0.13, 0);
+       
+        this.bg_trees1 = this.add.image(0, 0, 'bg_trees1').setOrigin(0.134, 0);
+        this.bg_terrain_front = this.add.image(0, 0, 'bg_terrain_front').setOrigin(0, 0.17);
+        this.bg_trees_front = this.add.image(0, 0, 'bg_trees_front').setOrigin(0, 0.17);
+        this.bg_cloud5 = this.add.image(0, 0, 'bg_cloud5').setOrigin(0.05, 0.1);
+       
+
+
+       
         // Het morfit logo aan de bovenkant
         this.boot_morfitLogo = this.add.image(0, 0, 'boot_morfitLogo').setOrigin(0, 0);
         this.grid.placeAtIndex(2, this.boot_morfitLogo); 
@@ -137,50 +142,58 @@ class BootScene extends Phaser.Scene {
         // Beide vogels die in de lucht zweven
         this.boot_bird1 = this.add.sprite(0, 0, 'boot_bird1', 0);
         this.boot_bird1.play('fly');
-        this.grid.placeAtIndex(24, this.boot_bird1);
+        this.grid.placeAtIndex(35, this.boot_bird1);
         this.grid.scaleTo(this.boot_bird1, 0.3, 500);
         this.boot_bird2 = this.add.sprite(0, 0, 'boot_bird2');
         this.boot_bird2.play('fly2');
-        this.grid.placeAtIndex(41, this.boot_bird2);
+        this.grid.placeAtIndex(63, this.boot_bird2);
         this.grid.scaleTo(this.boot_bird2, 0.3, 500);
-        // Boom aan de linkerkant van het scherm
+        // Plant aan de rechter van het scherm
         this.boot_plant2 = this.add.image(0, 0, 'boot_plant2');
-        this.grid.placeAtIndex(86, this.boot_plant2);
-        this.grid.scaleTo(this.boot_plant2, 0.2, 500);
+        this.grid.placeAtIndex(108, this.boot_plant2);
+        this.grid.scaleTo(this.boot_plant2, 0.17, 500);
         // De grond
         this.boot_ground = this.add.image(0, this.sys.game.config.height, 'boot_ground').setOrigin(0, 1);
         this.grid.scaleY(this.boot_ground, .22);
-        // Plant aan de rechterkant van het scherm
+        // Boom aan de linkerkant van het scherm
         this.boot_plant1 = this.add.image(0, 0, 'boot_plant1');
-        this.grid.placeAtIndex(67, this.boot_plant1);
-        this.grid.scaleTo(this.boot_plant1, 0.35, 500);
+        this.grid.placeAtIndex(89, this.boot_plant1);
+        this.grid.scaleTo(this.boot_plant1, 0.30, 500);
         // De morfit in het midden van het scherm
         this.boot_morfitWalking = this.add.image(0, 0, 'boot_morfitWalking').setScale(0.35);
-        this.grid.placeAtIndex(71, this.boot_morfitWalking);
+        this.grid.placeAtIndex(82, this.boot_morfitWalking);
         // De laad tekst onderaan het scherm
-        this.boot_loadingText = this.add.image(0, 0, 'boot_loadingText');
-        this.grid.placeAtIndex(115, this.boot_loadingText);
-        this.grid.scaleTo(this.boot_loadingText, .9, 500);
+        this.boot_loadingText = this.add.text(0, 0, 'Je Morfit is onderweg', { fontFamily: 'Bubblegum Sans', fontSize: '34px', fill: 'white'}).setOrigin(0.5, 0.5);
+        this.grid.placeAtIndex(137, this.boot_loadingText);
+        
+
+
+        // this.boot_loadingText = this.add.image(0, 0, 'boot_loadingText');
+        // this.grid.placeAtIndex(115, this.boot_loadingText);
+        // this.grid.scaleTo(this.boot_loadingText, .9, 500);
         // this.grid.showNumbers();
 
     
         
     }
-    
 
     overview_assets() {
         this.load.image('bg', 'src/assets/boot/bg/background.png');
         this.load.image('bg_mntn1', 'src/assets/boot/bg/mountain-depth-5.png');
         this.load.image('bg_mntn2', 'src/assets/boot/bg/mountain-depth-4.png');
         this.load.image('bg_mntn3', 'src/assets/bg/mountain-depth-3.png');
-        this.load.image('bg_mntn2', 'src/assets/bg/Mountain-depth-2.png');
+        this.load.image('bg_terrain_front', 'src/assets/boot/bg/terrain-front.png');
+        this.load.image('bg_trees_front', 'src/assets/boot/bg/trees-front.png');
         this.load.image('bg_trees1', 'src/assets/boot/bg/trees-depth-4.png');
+        this.load.image('bg_cloud6', 'src/assets/boot/bg/clouds-depth-6.png');
+        this.load.image('bg_cloud5', 'src/assets/boot/bg/clouds-depth-5.png');
         this.load.image('sun', 'src/assets/bg/Sun.png');
         this.load.image('bg_sky', 'src/assets/bg/Sky cloud.png');
     //     this.load.image('bg_mntn1', 'src/assets/bg/home/morfit walking.png');
     //     this.load.image('bg_mntn0', 'src/assets/bg/home/boot_ground.png');
      }
-
 }
+
+
 
 export default BootScene;
