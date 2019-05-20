61b97bf5f1bcfa772e33e44e1cb9a7b610d7241e
Guido van Dijk <GuidoD@Guido-van-Dijk.local>
2019-05-20 17:37:40 +0200

    Added unhealty food to boot load

--- src/boot/games/_RunMorfiRun.js
+++ src/boot/games/_RunMorfiRun.js
@@ -4,4 +4,5 @@ export function game_RunMorfiRun_assets() {
     this.load.image('games:platform', 'src/assets/games/run_morfi_run/platform.png');
     this.load.image('games:finish', 'src/assets/games/run_morfi_run/finish.png');
     this.load.image('games:ground', 'src/assets/games/run_morfi_run/ground.png');
+    this.load.image('games:hamburger', 'src/assets/illustraties/food/unhealty/Hamburger.png');
 }
