442b372e764150150bedbfa800091fe529ea7282
Guido van Dijk <GuidoD@Guido-van-Dijk.local>
2019-05-21 12:46:58 +0200

    Loaded all images

--- src/boot/games/_RunMorfiRun.js
+++ src/boot/games/_RunMorfiRun.js
@@ -4,5 +4,9 @@ export function game_RunMorfiRun_assets() {
     this.load.image('games:platform', 'src/assets/games/run_morfi_run/platform.png');
     this.load.image('games:finish', 'src/assets/games/run_morfi_run/finish.png');
     this.load.image('games:ground', 'src/assets/games/run_morfi_run/ground.png');
+    this.load.image('games:bg', 'src/assets/games/run_morfi_run/bg.png');
+    this.load.image('games:you', 'src/assets/games/run_morfi_run/you.png');
+    this.load.image('games:enemy', 'src/assets/games/run_morfi_run/enemy.png');
+    this.load.image('games:finish-icon', 'src/assets/games/run_morfi_run/finish-icon.png');
     this.load.image('games:hamburger', 'src/assets/illustraties/food/unhealty/Hamburger.png');
 }
