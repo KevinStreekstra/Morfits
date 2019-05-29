a33ea44a38eda337e63aa5855ed10b77c08a0470
Patrick Frumau <pariek@Patricks-MacBook-Pro.local>
2019-05-29 08:54:48 +0200

    fixed merge conflicts

--- src/scenes/Overview.js
+++ src/scenes/Overview.js
@@ -77,7 +77,8 @@ class OverviewScene extends Phaser.Scene {
             this.scene.launch('StatsScene');
         });
         this.grid.placeAtIndex(3, this.whiteBg);
-        this.Mylevel = this.add.text(0, 0, this._player.getLevel(), { fontFamily: 'Bubblegum Sans', fontSize: `${Math.round(18 * window.devicePixelRatio)}px`, fill: 'black'});
+        this.Mylevel = this.add.text(0, 0, this._player.getLevel(), { fontFamily: 'Bubblegum Sans', fontSize: `${Math.round(18 * window.devicePixelRatio)}px`, fill: 'black'}).setOrigin(1.4, 0.2);
+        this.grid.placeAtIndex(1, this.Mylevel);
         this.Myname = this.add.text(0, 0, this.player.username, { fontFamily: 'Bubblegum Sans', fontSize: `${Math.round(16 * window.devicePixelRatio)}px`, fill: 'black', Align: 'center'}).setOrigin(0.6, 0.25);
         this.grid.placeAtIndex(3, this.Myname);
         this.navPP = this.addImage(0, 0, 'overview:navPP').setOrigin(0.8, 0.43);
@@ -148,10 +149,10 @@ class OverviewScene extends Phaser.Scene {
         this.grid.placeAtIndex(127, this.morfitWalking);
 
         this.txtXPbar = this.add.text(0, 0, `${this.player.xp}/${this._player.getRequiredXP(this._player.getLevel())}`, { fontFamily: 'Bubblegum Sans', fontSize: `${Math.round(10 * window.devicePixelRatio)}px`});
-        Phaser.Display.Align.In.Center(this.txtXPbar, this.bg, 0, 375 );
+        this.grid.placeAtIndex(171, this.txtXPbar);
 
-        this.XPbar = this.addImage(0, 0, 'overview:XPbar');
-        Phaser.Display.Align.In.Center(this.XPbar, this.bg, 0, 395);
+        this.XPbar = this.addImage(0, 0, 'overview:XPbar').setOrigin(0.28, 0);
+        this.grid.placeAtIndex(171, this.XPbar);
 
         this.HomePlant = this.addImage(0, 0, 'overview:HomePlant2');
         this.grid.placeAtIndex(164, this.HomePlant);
@@ -199,17 +200,11 @@ class OverviewScene extends Phaser.Scene {
         this.grid.placeAtIndex(25, this.IconPower);
 
 
-        //this.grid.showNumbers();
+        this.grid.showNumbers();
     }
 
     update() {
-        this.Mylevel.text = this._player.getLevel();
-        Phaser.Display.Align.In.Center(
-            this.Mylevel,
-            this.whiteBg,
-            -110,
-            7      
-        );
+        this.Mylevel.text = this._player.getLevel();  
         this.MyPP.text = this._player.getPowerPoints();
         this.MyM_dollars.text = this._player.getMorfos();
  
