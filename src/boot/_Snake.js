e412f178334087e59e724dd8e182a021ce1e76ec
Yannick Frisart <yannick.frisart@outlook.com>
2019-06-02 17:52:49 +0200

    #69 Improved filenaming

--- src/boot/_Snake.js
+++ src/boot/_Snake.js
@@ -2,7 +2,7 @@ export function snake_assets() {
     // Snake assets
     this.load.image('Snake:player1_body', require('../assets/snake/body/body-part-player-1.svg'));
     this.load.image('Snake:player2_body', require('../assets/snake/body/body-part-player-2.svg'));
-    this.load.image('Snake:player1_head', require('../assets/snake/Head.png'));
+    this.load.image('Snake:player1_head', require('../assets/snake/body/head.png'));
 
     // Board assets
     this.load.image('Snake:board_background', require('../assets/snake/board/field.png'));
@@ -15,10 +15,10 @@ export function snake_assets() {
 
     // End game assets
     this.load.image('Snake:head_player_1', require('../assets/snake/ui/end_game/player_head.png'))
-    this.load.image('Snake:medal_first', require('../assets/snake/ui/end_game/first.png'))
-    this.load.image('Snake:medal_second', require('../assets/snake/ui/end_game/second.png'))
-    this.load.image('Snake:medal_third', require('../assets/snake/ui/end_game/third.png'))
-    this.load.image('Snake:morfit_coin', require('../assets/snake/ui/end_game/assets_dollar.png'))
+    this.load.image('Snake:medal_first', require('../assets/snake/ui/end_game/medal_first.png'))
+    this.load.image('Snake:medal_second', require('../assets/snake/ui/end_game/medal_second.png'))
+    this.load.image('Snake:medal_third', require('../assets/snake/ui/end_game/medal_third.png'))
+    this.load.image('Snake:morfit_coin', require('../assets/snake/ui/end_game/dollar_icon.png'))
     this.load.image('Snake:home_button', require('../assets/snake/ui/end_game/home_button.png'))
 
 
