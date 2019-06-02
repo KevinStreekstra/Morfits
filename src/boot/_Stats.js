ec594960df90228e4161eb09eea80019ed1392f1
Yannick Frisart <yannick.frisart@outlook.com>
2019-06-02 17:35:10 +0200

    #69 Updated stats file names

--- src/boot/_Stats.js
+++ src/boot/_Stats.js
@@ -1,17 +1,22 @@
 export function stats() {
-    this.load.image('Stats:bg', require('../assets/stats/background@2x.png'));
-    this.load.image('Stats:TabBg', require('../assets/stats/tab_background_states@2x.png'));
-    this.load.image('Stats:plank_1', require('../assets/stats/plank_1@2x.png'));
-    this.load.image('Stats:Vines', require('../assets/stats/vines@2x.png'));
-    this.load.image('Stats:levelIndicatorBG', require('../assets/stats/level_indicator@2x.png'));
-    this.load.image('Stats:XPBar', require('../assets/stats/exp_bar@2x.png'));
+    // Background elements
+    this.load.image('Stats:bg', require('../assets/stats/backgrounds/background.png'));
+    this.load.image('Stats:TabBg', require('../assets/stats/backgrounds/modal_background.png'));
+    this.load.image('Stats:plank_1', require('../assets/stats/backgrounds/plank_background.png'));
+    this.load.image('Stats:Vines', require('../assets/stats/backgrounds/vines.png'));
+    this.load.image('Stats:levelIndicatorBG', require('../assets/stats/backgrounds/level_indicator_background.png'));
 
-    this.load.image('Stats:SmallHeart', require('../assets/stats/icon_mental_fill_white.png'));
-    this.load.image('Stats:SmallEnergy', require('../assets/stats/icon_energy_fill_white.png'));
-    this.load.image('Stats:SmallPower', require('../assets/stats/icon_kracht_fill_white.png'));
+    // Bars
+    this.load.image('Stats:XPBar', require('../assets/stats/bars/experience_bar.png'));
 
-    this.load.image('Stats:BigHeart', require('../assets/stats/icon_mental_fill_white@2x.png'));
-    this.load.image('Stats:BigEnergy', require('../assets/stats/icon_energy_fill_white@2x.png'));
-    this.load.image('Stats:BigPower', require('../assets/stats/icon_kracht_fill_white@2x.png'));
+    // Small icons
+    this.load.image('Stats:SmallHeart', require('../assets/stats/icons/mentality_icon_small.png'));
+    this.load.image('Stats:SmallEnergy', require('../assets/stats/icons/energy_icon_small.png'));
+    this.load.image('Stats:SmallPower', require('../assets/stats/icons/power_icon_small.png'));
+
+    // Big icons
+    this.load.image('Stats:BigHeart', require('../assets/stats/icons/mentality_icon_big.png'));
+    this.load.image('Stats:BigEnergy', require('../assets/stats/icons/energy_icon_big.png'));
+    this.load.image('Stats:BigPower', require('../assets/stats/icons/power_icon_big.png'));
 
 }
