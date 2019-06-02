a3d24ab19dc963273f85071ca9f79249fdd00902
Yannick Frisart <yannick.frisart@outlook.com>
2019-06-02 19:19:47 +0200

    #69 Improved filenaming of bonus question

--- src/boot/BootQuiz/_Question5.js
+++ src/boot/BootQuiz/_Question5.js
@@ -1,4 +1,4 @@
 export function Question5() {
-    this.load.image('Question5:answer2', require('../../assets/daily_reward_quiz/daily_reward_quiz_q_bonus/quiz_answer_2@2x.png'));
-    this.load.image('Question5:answer1', require('../../assets/daily_reward_quiz/daily_reward_quiz_q_bonus/quiz_answer_1@2x.png'));
+    this.load.image('Question5:answer1', require('../../assets/daily_reward/quiz/bonus/answer_calories_fat.png'));
+    this.load.image('Question5:answer2', require('../../assets/daily_reward/quiz/bonus/answer_calories_movement.png'));
 }
